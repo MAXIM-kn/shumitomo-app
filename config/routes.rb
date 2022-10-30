ca3f69cc62de8ded926f882c3e6dd941bc54f2f4
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-10-30 13:58:46 +0900

    カテゴリーコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
   resources :users, only: :show
-  resources :chat_rooms, only: [:new, :create]
+  resources :chat_rooms, only: [:index, :new, :create]
 end
