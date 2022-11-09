974e97dfdf7e5640dd8c122e9d6b0c5aec6d063e
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-09 16:39:36 +0900

    チャットページの編集

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -2,6 +2,7 @@ class ChatMessagesController < ApplicationController
 
   def index
     @chat_room_user = ChatRoomUser.where(params[:id])
+    @chat_room = ChatRoom.find_by(params[:id])
   end
 
 end
