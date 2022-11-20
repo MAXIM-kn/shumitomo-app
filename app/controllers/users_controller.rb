a2f5f447714ba62bc8e0be63caeebb302841bbb3
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-20 11:15:10 +0900

    user編輯機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :set_follow, only: [:follows, :followers]
   
   def show
@@ -20,6 +21,11 @@ class UsersController < ApplicationController
   end
   
   def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,6 +34,10 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users_following = user.following_user.page(params[:page]).per(20).order('updated_at DESC')
     @users_follower = user.follower_user.page(params[:page]).per(20).order('updated_at DESC')
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :image, :profile)
   end
 
 end
