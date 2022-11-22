b830d553829e971455c801b1262dcbaad76b19de
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-22 14:51:04 +0900

    チャットルーム曜日の表示

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,2 @@
-wd = ["日", "月", "火", "水", "木", "金", "土"]
-time = Time.now
-Time::DATE_FORMATS[:datetime_jp] = time.strftime("%m/%d %H:%M (#{wd[time.wday]})")
+Time::DATE_FORMATS[:datetime_jp] = '%m月 %d日 %H時 %M分'
+Time::DATE_FORMATS[:wday_jp] = '%w'
