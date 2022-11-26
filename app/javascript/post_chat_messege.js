c2349d3ad778767962c9e498327cedbc5f614a36
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-26 12:09:15 +0900

    DMのAjax機能の実装

--- app/javascript/post_chat_messege.js
+++
@@ -1,9 +0,0 @@
-function messeage_post (){
-  const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    const formText = document.getElementById("content");
-  });
- };
- 
- window.addEventListener('load', messeage_post);
