71cf8b05e30ab28ab906542f4d489ee1bbe76feb
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-27 13:39:13 +0900

    いいね機能Ajax機能の導入

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,26 @@ class LikesController < ApplicationController
   def create
     @chat_room = ChatRoom.find(params[:chat_room_id])
     @chat_room_like = Like.new(user_id: current_user.id, chat_room_id: params[:chat_room_id])
-    if @chat_room_like.save
-      redirect_to chat_room_path(@chat_room)
-    else
-      redirect_to root_path
+    respond_to do |format|
+      if @chat_room_like.save
+        format.html { redirect_to chat_room_path(@chat_room) }
+        format.js
+      else
+        redirect_to root_path
+      end
     end
   end
 
   def destroy
     @chat_room = ChatRoom.find(params[:chat_room_id])
     @chat_room_like = Like.find_by(user_id: current_user.id, chat_room_id: params[:chat_room_id])
-    if @chat_room_like.destroy
-      redirect_to chat_room_path(@chat_room)
-    else
-      redirect_to root_path
+    respond_to do |format|
+      if @chat_room_like.destroy
+        format.html { redirect_to chat_room_path(@chat_room) }
+        format.js
+      else
+        redirect_to root_path
+      end
     end
   end
 
