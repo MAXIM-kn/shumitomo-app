026eb90201cc64c057181ae6dfd426c1a7a4effc
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-12-04 22:56:47 +0900

    フォロー機能Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,31 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
 
   def create
-    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    @user.create_notification_follow!(current_user)
-    redirect_to request.referer
+    respond_to do |format|
+      @user.create_notification_follow!(current_user)
+      @following_users = @user.following_user
+      @follower_users = @user.follower_user
+      format.html { redirect_to request.referer }
+      format.js 
+    end
   end
   
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    respond_to do |format|
+      current_user.unfollow(params[:user_id])
+      @following_users = @user.following_user
+      @follower_users = @user.follower_user
+      format.html { redirect_to request.referer }
+      format.js 
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
   
 end
