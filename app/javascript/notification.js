43ad50e82bfc93c390ff6bbdb2ae26244745437a
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-12-08 18:28:05 +0900

    ブックマーク機能の実装

--- app/javascript/notification.js
+++ app/javascript/notification.js
@@ -1,15 +1,37 @@
 const notification = () => {
+  const bookmarkButton = document.getElementById("bookmark")
   const notificationButton = document.getElementById("notification-btn")
   const notificationWrap = document.getElementById("notification-wrap")
+  const bookmarkWrap = document.getElementById("bookmark-wrap")
   const returnButton = document.getElementById("return-btn")
+  const rreturnBookmarkButton = document.getElementById("return-btn-bookmark")
 
   notificationButton.addEventListener('click', function() {
-    notificationWrap.setAttribute("style", "display:block;")
+    if (bookmarkWrap.getAttribute("style") == "display:block;") {
+      bookmarkWrap.removeAttribute("style")
+      notificationWrap.setAttribute("style", "display:block;")
+    } else {
+      notificationWrap.setAttribute("style", "display:block;")
+    }
+  });
+
+  bookmarkButton.addEventListener('click', function() {
+    if (notificationWrap.getAttribute("style") == "display:block;") {
+      notificationWrap.removeAttribute("style")
+      bookmarkWrap.setAttribute("style", "display:block;")
+    } else {
+      bookmarkWrap.setAttribute("style", "display:block;")
+    }
   });
 
   returnButton.addEventListener('click', function() {
     notificationWrap.removeAttribute("style")
   });
+
+  rreturnBookmarkButton.addEventListener('click', function() {
+    bookmarkWrap.removeAttribute("style")
+  });
+
 };
 
 window.addEventListener('load', notification)
